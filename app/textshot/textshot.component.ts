a0da737f757b7eb5601c13dd0b87cbd91875f9d5
Harry Jubb <harry.jubb@sanger.ac.uk>
2016-05-14 01:51:51 +0100

    Very basic beginnings!

---
+++ app/textshot/textshot.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ts-textshot',
+  templateUrl: 'app/textshot/textshot.template.html'
+})
+export class TSTextShotComponent { }
