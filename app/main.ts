b639f22c7be26e9f09c4966094309368bf597c2a
Harry Jubb <harry.jubb@sanger.ac.uk>
2016-05-14 02:46:49 +0100

    Basic TextShot preview.

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,7 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
+// Add all operators to Observables
+import 'rxjs/Rx';
 
+import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { TSAppComponent } from './app.component';
 
 bootstrap(TSAppComponent);
--- app/textshot/textshot.component.ts
+++ app/textshot/textshot.component.ts
@@ -1,7 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Control } from '@angular/common';
+
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'ts-textshot',
   templateUrl: 'app/textshot/textshot.template.html'
 })
-export class TSTextShotComponent { }
+export class TSTextShotComponent implements OnInit {
+
+  textShotInputControl = new Control();
+  textShotText: string = "";
+
+  constructor () {
+
+  }
+
+  ngOnInit () {
+
+    // TEXTSHOT DEBOUNCE
+    // this.result = this.textShotInputControl.valueChanges
+    //                     .debounceTime(400)
+    //                     .distinctUntilChanged()
+    //                     .switchMap(text => null);
+
+  }
+
+}
