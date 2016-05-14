c8380218762786fab41dd00a6937233cbea0597f
Harry Jubb <harry.jubb@sanger.ac.uk>
2016-05-14 09:09:15 +0100

    TextShot is now downloadable. Problem with download file naming.

--- app/textshot/textshot.component.ts
+++ app/textshot/textshot.component.ts
@@ -13,6 +13,8 @@ export class TSTextShotComponent implements OnInit {
   textShotText: string = "";
   textShotIsGenerated: boolean = false;
   textShotPNGUrl: string = "";
+  textShotPNGOctetUrl: string = "";
+  textShotPNGBlobUrl: string = "";
   previewText: string = "PREVIEW";
 
   constructor () {
@@ -59,12 +61,44 @@ export class TSTextShotComponent implements OnInit {
     html2canvas(document.getElementById('textshot-div'), {
         onrendered: function(canvas) {
             // CANVAS IS THE FINAL RENDERED <CANVAS> ELEMENT
-            base.textShotIsGenerated = true;
             base.previewText= "TEXTSHOT GENERATED!";
             base.textShotPNGUrl = canvas.toDataURL(); // PNG BY DEFAULT
+            base.textShotPNGOctetUrl = base.textShotPNGUrl.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 
-            // console.log(pngUrl);
-            // document.body.appendChild(canvas);
+            // MAKE DOWNLOAD BLOB
+            // CURRENTLY DOESN'T WORK
+            // SEE http://stackoverflow.com/questions/10473932/browser-html-force-download-of-image-from-src-dataimage-jpegbase64
+
+            // var imageData = atob(base.textShotPNGUrl.split(',')[1]);
+            // // Use typed arrays to convert the binary data to a Blob
+            // var arrayBuffer = new ArrayBuffer(imageData.length);
+            // var view = new Uint8Array(arrayBuffer);
+            // for (var i=0; i<imageData.length; i++) {
+            //     view[i] = imageData.charCodeAt(i) & 0xff;
+            // }
+            //
+            // var blob: Blob;
+            //
+            // try {
+            //     // This is the recommended method:
+            //     blob = new Blob([arrayBuffer], {type: 'application/octet-stream'});
+            // } catch (e) {
+            //     // The BlobBuilder API has been deprecated in favour of Blob, but older
+            //     // browsers don't know about the Blob constructor
+            //     // IE10 also supports BlobBuilder, but since the `Blob` constructor
+            //     //  also works, there's no need to add `MSBlobBuilder`.
+            //     var bb = new (window.WebKitBlobBuilder || window.MozBlobBuilder);
+            //     bb.append(arrayBuffer);
+            //     blob = bb.getBlob('application/octet-stream'); // <-- Here's the Blob
+            // }
+            //
+            // // Use the URL object to create a temporary URL
+            // var blobUrl = (window.webkitURL || window.URL).createObjectURL(blob);
+            // console.log(blobUrl);
+            // base.textShotPNGBlobUrl = blobUrl;
+
+            base.textShotIsGenerated = true;
+
         }
     });
 
@@ -72,6 +106,9 @@ export class TSTextShotComponent implements OnInit {
 
   downloadTextShotImage () {
     console.log('Downloading...');
+    var url = this.textShotPNGUrl.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
+    // var url = this.textShotPNGUrl;
+    window.open(url);
   }
 
 }
