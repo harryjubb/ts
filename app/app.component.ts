07e79a1d04c4583475afb27415128bc365395585
Harry Jubb <harry.jubb@sanger.ac.uk>
2016-05-14 01:19:24 +0100

    More setup of TypeScript Angular2.

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ts-app',
+  templateUrl: 'app/app.template.html'
+})
+export class TSAppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { TSAppComponent } from './app.component';
+
+bootstrap(TSAppComponent);
