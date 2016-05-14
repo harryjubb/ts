a8974043b049e24c1371b1eae1f66194684b14f0
Harry Jubb <harry.jubb@sanger.ac.uk>
2016-05-14 02:04:32 +0100

    More styles and added first inner component.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
 
+import { TSTextShotComponent } from './textshot/textshot.component';
+
 @Component({
   selector: 'ts-app',
-  templateUrl: 'app/app.template.html'
+  templateUrl: 'app/app.template.html',
+  directives: [
+    TSTextShotComponent
+  ]
 })
 export class TSAppComponent { }
